1c3a1222454889519563313a291976d3eafc82cc
Wouter Felius <wfelius@gmail.com>
2014-10-24 17:21:18 +0200

    fixed login

--- www/login.php
+++ www/login.php
@@ -1,5 +1,5 @@
 <h1>Log in</h1>
-<form method="POST" action="index.php?page=home">
+<form method="POST" action="auth.php">
 <fieldset>
 	<label for='username'>Username</label>
 	<input type="text" name="username" id="username">
