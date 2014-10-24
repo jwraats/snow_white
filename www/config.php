1bf4378a4be9254dbf1fa5924af8e1b01ce48386
Jayce Ardon <jayceardon@gmail.com>
2014-10-24 17:26:29 +0200

    select queries

--- www/config.php
+++ www/config.php
@@ -6,11 +6,7 @@ $db_user = 'root';
 $db_pass = '';
 
 try {
-    $dbh = new PDO('mysql:host='.$db_host.';dbname='.$db_name, $db_user, $db_pass);
-    foreach($dbh->query('SELECT * from user') as $row) {
-        print_r($row);
-    }
-    $dbh = null;
+    $pdo = new PDO('mysql:host='.$db_host.';dbname='.$db_name, $db_user, $db_pass);
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
--- www/detail.php
+++ www/detail.php
@@ -1,3 +1,65 @@
 <?php
+echo $_GET['id'];
+$user_id = isset($_GET['id']) ? $_GET['id'] : 'login';
+$select_detail = $pdo->prepare('SELECT u.* FROM user u 
+								INNER JOIN user f ON u.follow_id = f.id
+								WHERE u.id = :user_id');
 
-echo "Whatch you video 'n stuff";
+$follow_detail = $pdo->prepare('SELECT f.* FROM user u 
+								INNER JOIN user f ON u.follow_id = f.id
+								WHERE u.id = :user_id');
+
+try
+{	
+	$select_detail->execute(array(':user_id' => $user_id));
+	$detail = $select_detail->fetchAll();
+	$select_detail->closeCursor();
+}
+catch(PDOException $e)
+{
+	$json['error'] = TRUE;
+	$json['msg'] = 'Error executing select_detail: ' . $e->getMessage();
+	exit;
+}
+
+try
+{	
+	$follow_detail->execute(array(':user_id' => $user_id));
+	$follow = $follow_detail->fetchAll();
+	$follow_detail->closeCursor();
+}
+catch(PDOException $e)
+{
+	$json['error'] = TRUE;
+	$json['msg'] = 'Error executing follow_detail: ' . $e->getMessage();
+	exit;
+}
+
+if(count($detail) > 0){
+	foreach($detail as $row){
+		echo $row["first_name"];
+		echo "<br/>";
+		echo $row["last_name"];
+		echo "<br/>";
+		echo $row["username"];
+		echo "<br/>";
+		echo $row["picture"];
+		echo "<br/>";
+	}
+} else {
+	echo 'geen resultaten';
+}
+
+if(count($follow) > 0){
+	foreach($follow as $row){
+		echo $row["first_name"];
+		echo "<br/>";
+		echo $row["last_name"];
+		echo "<br/>";
+		echo $row["username"];
+		echo "<br/>";
+		echo $row["picture"];
+	}
+} else {
+	echo 'geen resultaten';
+}
