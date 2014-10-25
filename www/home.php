d2ae437a69d344482500ca9e79d564c19f14581c
Jan Willem Raats <jw.raats@gmail.com>
2014-10-25 08:41:04 +0200

    Home fix and profile picture auto update with kinect

--- www/home.php
+++ www/home.php
@@ -14,6 +14,12 @@ if(!$detail){
 	echo "User not existing";
 	exit;
 }
+
+$profileImage = "/images/".$detail->id."_0_default.jpeg";
+if(!file_exists(".".$profileImage)){
+	$profileImage = "/assets/img/avatar.png";
+}
+
 $feeds = $db->getFeedByUserId($detail->id);
 if($feeds){
 	$countFeeds = count($feeds);
@@ -52,20 +58,22 @@ else{
 		<div class="container">
 			<a class="trigger-menu" href="#"></a>
 			<a class="trigger-settings" href="#"></a>
+		</div>
+	</div>
 	<!--avatar-->
 	<header class="profile">
-		<img src="<?php echo $detail->picture; ?>" class="profile-avatar"/>
+		<img src="<?php echo $profileImage;  ?>" class="profile-avatar"/>
 		<h1><?php echo $detail->first_name; ?> <?php echo $detail->last_name; ?></h1>
 		<h2><?php echo $detail->description; ?></h2>
 		<div class="full">
 			<div class="third">
-				<a href="./index.php?page=home" class="profile video"><?php echo $countFeeds; ?></a>
+				<a href="./index.php?page=home<?php if(isset($_GET['id'])){ echo "&id=".$_GET['id']; } ?>" class="profile video"><?php echo $countFeeds; ?></a>
 			</div>
 			<div class="third">
-				<a href="./index.php?page=home&friends" class="profile photo"><?php echo $countFriends; ?></a>
+				<a href="./index.php?page=home&friends<?php if(isset($_GET['id'])){ echo "&id=".$_GET['id']; } ?>" class="profile photo"><?php echo $countFriends; ?></a>
 			</div>
 			<div class="third">
-				<a href="./index.php?page=home&likes" class="profile likes">5531</a>
+				<a href="./index.php?page=home&likes<?php if(isset($_GET['id'])){ echo "&id=".$_GET['id']; } ?>" class="profile likes">5531</a>
 			</div>
 		</div>
 	</header>
